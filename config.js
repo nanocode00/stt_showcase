aed0e8ae248bd3a62773fa202a064f7d8d54c5f1
JaeHun Kim <101373394+nanocode00@users.noreply.github.com>
2026-08-24 09:32:58 +0900

    feat: 실시간 마이크 STT 쇼케이스 추가

---
+++ config.js
@@ -0,0 +1,6 @@
+window.MALLO_SHOWCASE_CONFIG = Object.freeze({
+  // 공개 backend가 정해지면 HTTPS origin만 입력합니다. 끝의 /는 생략합니다.
+  API_BASE_URL: "https://murphy-belongs-behavioral-minimize.trycloudflare.com",
+  REQUEST_TIMEOUT_MS: 90000,
+  MAX_RECORDING_SEC: 20,
+});
